c72245df38364f3ed94c48c95ebbeedbd0766317
Tyler Maxwell <tylermaxwell661@gmail.com>
2019-05-28 16:18:54 -0700

    Updates

---
+++ precache-manifest.0029f07c3e146eb543773220d27d8d2e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6434194b28281c89f4806c1a636ae82",
+    "url": "/pokemon/index.html"
+  },
+  {
+    "revision": "ea24d29783a1c6f0899e",
+    "url": "/pokemon/static/css/main.90a80858.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/pokemon/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "ea24d29783a1c6f0899e",
+    "url": "/pokemon/static/js/main.e8f0fae1.chunk.js"
+  },
+  {
+    "revision": "b04917ec47d1c084d1e9",
+    "url": "/pokemon/static/js/runtime~main.0909cbae.js"
+  }
+]);
